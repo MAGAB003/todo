85f1cc161240279c81ce0851e3fd39e7e8970ccf
HM\MAGAO <maja.gabrielsson@hm.com>
2020-12-03 11:51:55 +0100

    added class Item and repository

---
+++ src/main/java/com/example/todoProject/Item.java
@@ -0,0 +1,75 @@
+package com.example.todoProject;
+
+public class Item {
+    private String name;
+    private Integer ID;
+    private Boolean done;
+    private String category;
+    private String description;
+    private String updatedAt;
+    private String createdAt;
+
+    public Item(String name, String category, String description) {
+        this.name = name;
+        this.category = category;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+
+}
---
+++ src/main/java/com/example/todoProject/ToDoRepository.java
@@ -0,0 +1,8 @@
+package com.example.todoProject;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ToDoRepository {
+
+}
